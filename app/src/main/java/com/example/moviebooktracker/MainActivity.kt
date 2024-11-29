99422d7baa02d2a785d0045055afc04876dcb0ce
Łukasz <ukasz@>
2024-11-29 12:03:58 +0100

    zaczecie aplikacji

--- app/src/main/java/com/example/moviebooktracker/MainActivity.kt
+++ app/src/main/java/com/example/moviebooktracker/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.moviebooktracker
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+       val MyButton =  findViewById<Button>(R.id.save_data)
+
+        MyButton.setOnClickListener{
+
         }
     }
 }
